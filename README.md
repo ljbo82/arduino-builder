d1bb6ddc8fab656df2f150f101a0a6ed20d22f91
Leandro Oliveira <leandro@agapsys.com>
2022-06-12 00:56:14 -0300

    General review

---
+++ README.md
@@ -0,0 +1,15 @@
+# arduino-builder
+
+arduino builder provides a build system created on top of [gcc-project-builder](https://github.com/ljbo82/gcc-project-builder) in order to build arduino projects.
+
+> **NO OFFICIAL RELEASE YET**
+>
+> arduino-builder is still under development and there is no official releases yet!
+
+## License
+
+arduino-builder is distributed under MIT License. Please see the [LICENSE](LICENSE) file for details on copying and distribution.
+
+## Usage
+
+See [gcc-project-builder](https://github.com/ljbo82/gcc-project-builder).
---
+++ hosts/arduino-avr/host.mk
@@ -0,0 +1,84 @@
+# Copyright (c) 2022 Leandro José Britto de Oliveira
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# arduino-avr host definitions
+
+ifndef __arduino_avr_mk__
+__arduino_avr_mk__ := 1
+
+CROSS_COMPILE  ?= avr-
+
+ifndef LIB_TYPE
+    LIB_TYPE := static
+else
+    ifneq ($(LIB_TYPE),static)
+        $(error [LIB_TYPE] Unsupported value: $(LIB_TYPE))
+    endif
+endif
+
+RELEASE_OPTIMIZATION_LEVEL := s
+
+ifdef ARDUINO_ARCH
+    $(error [ARDUINO_ARCH] Reserved variable)
+endif
+ARDUINO_ARCH := AVR
+
+override AS = $(CC)
+
+override CFLAGS   += -std=gnu11 -ffunction-sections -fdata-sections
+override CXXFLAGS += -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -Wno-error=narrowing
+override ASFLAGS  += -x assembler-with-cpp
+override LDFLAGS  += -Wl,--gc-sections
+
+ifeq ($(PROJ_TYPE),app)
+    override CFLAGS   += -flto -fno-fat-lto-objects
+    override CXXFLAGS += -flto
+    override ASFLAGS  += -flto
+    override LDFLAGS  += -flto -fuse-linker-plugin -mmcu=$(ARDUINO_MCU)
+endif
+
+override CFLAGS   += -mmcu=$(ARDUINO_MCU) -DF_CPU=$(ARDUINO_F_CPU) -DARDUINO_$(ARDUINO_BOARD) -DARDUINO_ARCH_$(ARDUINO_ARCH)
+override CXXFLAGS += -mmcu=$(ARDUINO_MCU) -DF_CPU=$(ARDUINO_F_CPU) -DARDUINO_$(ARDUINO_BOARD) -DARDUINO_ARCH_$(ARDUINO_ARCH)
+override ASFLAGS  += -mmcu=$(ARDUINO_MCU) -DF_CPU=$(ARDUINO_F_CPU) -DARDUINO_$(ARDUINO_BOARD) -DARDUINO_ARCH_$(ARDUINO_ARCH)
+
+ifeq ($(PROJ_TYPE),app)
+    define LAZY +=
+        POST_BUILD_DEPS  += $(O_BUILD_DIR)/$(ARTIFACT).hex
+        EXTRA_DIST_FILES += $(O_BUILD_DIR)/$(ARTIFACT).hex:bin/$(ARTIFACT).    hex
+
+        # =============================================================================
+        $(O_BUILD_DIR)/$(ARTIFACT).hex: $(O_BUILD_DIR)/$(ARTIFACT)
+	        @echo [HEX] $@
+	        $(O_VERBOSE)avr-objcopy -O ihex -R .eeprom $< $@
+        # =============================================================================
+
+        # =============================================================================
+        .PHONY: upload
+        upload: dist
+            ifeq ($(PORT),)
+	            $(error [PORT] Missing value)
+            endif
+	        @echo [UPLOAD] $(O_DIST_DIR)/bin/$(ARTIFACT).hex ==> $(PORT)
+	        $(O_VERBOSE)avrdude -C/etc/avrdude.conf -v -p$(ARDUINO_MCU) -carduino -P$(PORT) -Uflash:w:$(O_DIST_DIR)/bin/$(ARTIFACT).hex:i
+        # =============================================================================
+    endef
+endif
+
+endif # ifndef __arduino_avr_mk__
